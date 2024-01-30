21fdc5d9d2ee8d06835a022dfc24472f6d4d0744
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2024-01-30 22:23:15 +0530

    Update ParticipantItem.svelte

--- src/lib/components/ParticipantItem.svelte
+++ src/lib/components/ParticipantItem.svelte
@@ -3,6 +3,9 @@
     import {type Participant, ParticipantEvent, type Room} from "livekit-client";
     import {onMount, type SvelteComponent} from "svelte";
     import ParticipantVideo from "$lib/components/ParticipantVideo.svelte";
+    import {Button} from "$lib/components/ui/button";
+    import {faMicrophone, faMicrophoneSlash, faVideo, faVideoSlash} from "@fortawesome/free-solid-svg-icons";
+    import Icon from "svelte-awesome";
     export let participant: Participant;
     export let room: Room;
     let compRef: SvelteComponent;
@@ -28,19 +31,33 @@
 
     })
 
-    let color = "bg-accent";
+    let color = "bg-background";
     export function activate() {
         color = "bg-green-500";
     }
 
     export function deActivate() {
-        color = "bg-accent";
+        color = "bg-background";
+    }
+
+    function onMicButtonClick(){
+
+    }
+
+    function onVideoButtonClick() {
+
     }
 
 </script>
 
 
-<Card.Root class="w-full p-2 {color} " bind:this={compRef}>
+<Card.Root class="w-full p-2 {color} flex items-center justify-between" bind:this={compRef}>
     {participant.name}
+    <Button variant="ghost" class="ml-auto" on:click={onMicButtonClick}>
+        <Icon data={participant.isMicrophoneEnabled?faMicrophone:faMicrophoneSlash}  ></Icon>
+    </Button>
+    <Button variant="ghost" on:click={onVideoButtonClick}>
+        <Icon data={participant.isCameraEnabled?faVideo:faVideoSlash}></Icon>
+    </Button>
 </Card.Root>
 
